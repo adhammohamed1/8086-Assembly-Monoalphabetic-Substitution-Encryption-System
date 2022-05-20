5b24bbeb9d82448ea4a5c379c0be34edace2d8dd
adhammohamed1 <a.mohamed191100@gmail.com>
2022-05-20 22:21:01 +0200

    Major Overhaul
    Added an interactive UI and added loads of optimizations and bug fixes

--- README.md
+++ README.md
@@ -1,21 +1,58 @@
-# 8086-Assembly-Monoalphabetic-Substitution-Encryption-System
+# x86-Assembly-Monoalphabetic-Substitution-Encryption-System
 Monoalphabetic encryption/decryption system written in 8086 assembly language
 
-This assembly program can be run using the emulator emu8086.
-it encrypts/decrypts a given string according to the table below
+## Table of Content
+- [8086-Assembly-Monoalphabetic-Substitution-Encryption-System](#x86-assembly-monoalphabetic-substitution-encryption-system)
+  * [Abstract](#abstract)
+  * [Functionality](#functionality)
+  * [Tools](#tools)
+  * [Remarks](#remarks)
+  * [Sample Run Using emu8086](#sample-run)
+  
 
+## Abstract
+ Encryption can be done in various ways. One of the simplest methods is [**monoalphabetic cipher**](https://www.101computing.net/mono-alphabetic-substitution-cipher/). A monoalphabetic cipher is a substitution cipher where each letter of the plain text is replaced with another letter of the alphabet. It uses a fixed key which consist of the 26 letters of a “shuffled alphabet”.
+
+ This program implements this method to encrypt/decrypt a string of characters.
+
+ ## Functionality
+**1. This program uses the following table as the reference for character substitution**
 ![table](https://user-images.githubusercontent.com/90573502/168689119-19c527fd-0ec6-4235-9358-ff8b50976086.jpg)
 
-The program also omits any spaces in the result but I have clearly indicated the location of the part that does that in the code with comments so you can remove it at will.
+ **2. The program handles lowercase and uppercase characters**
 
-Sample run using emu8086:
+ **3. This program omits all spaces in the result, however, I've clearly indicated the location of the part that does that in the code with comments so you can remove it at will.**
 
-The string we will operate on is this:
-![string](https://user-images.githubusercontent.com/90573502/168689518-19cbcd9d-3d37-44b1-862d-4d3fc2cfa4ad.jpg)
+## Tools
+1. emu8086 (IDE)
+2. x86 Assembly
 
-Encrypted Message:
-![encrypted](https://user-images.githubusercontent.com/90573502/168689868-cb5c65e6-0381-4dc1-9d10-71b7c63d1645.jpg)
+## Remarks
+This assembly program was run and tested on the x86 emulator [emu8086](https://emu8086.en.lo4d.com/windows#:~:text=Tutorial....-,Emu8086%20is%20a%20Microprocessor%20Emulator%20with%20an%20integrated%208086%20Assembler,memory%20and%20input%2Foutput%20devices.).
 
-Encrypted then decrypted (spaces omitted):
-![decrypted](https://user-images.githubusercontent.com/90573502/168689917-f76e8bad-1e98-47bf-acd1-aaceccccaa52.jpg)
+
+## Sample Run Using emu8086:
+
+**The program lets the user choose whether to enter a string or use the hard-coded one**
+
+This is the harcoded string:
+![string](..%5C..%5Charcoded-string.jpg)
+
+**Main Menu**
+![main menu](..%5C..%5Cmainmenu.jpg)
+
+**Entered '2' to use the hard-coded string**
+![used](..%5C..%5Cused-hardcoded.jpg)
+
+**And done!**
+![done-hardcoded](..%5C..%5Cdone-hardcoded.jpg)
+
+**Entered 'y' to try again and '1' to input string**
+![entering_input](..%5C..%5Centering-input.jpg)
+
+**Processing the given string**
+![used-input](..%5C..%5Cused-input.jpg)
+
+**And done!**
+![done](..%5C..%5Cdone.jpg)
 
--- encrypt_decrypt.asm
+++ encrypt_decrypt.asm
@@ -1,163 +1,252 @@
 
-
-ORG 100H
-
+ORG 0100H
 
 JMP start
 
+newline                EQU 0AH   ; \n
+cret                   EQU 0DH   ; \r
+bcksp                  EQU 08H   ; \b
 
-newline EQU 0AH   ; \n
-cret    EQU 0DH   ; \r
+
+; 
+; Hard-coded string:
+;
+hardcoded_string       DB      'Oh hi there! This is an encrypted message', cret, newline, '$'
+
+
+input_string           DB      259 dup ('$')    ; Reserved area for input string (256 chars + \r + \n + $)                  
+                                                  
  
- 
-; String to be operated on
-string1 DB 'Oh hi there! this is an encrypted message', cret, newline, '$' ;'$' indicates the end of the string
+;                                                 
+; Messages and dialogues to be displayed in the UI:
+;
+message_welcome        DB      newline, 'Welcome to the monoalphabetic encryption system! ', cret, newline
+                       DB      'Please choose how you wish to proceed:', cret, newline
+                       DB      '1- Enter string as input (max: 256 chars)', cret, newline
+                       DB      '2- Use hard-coded string', cret, newline, '$'
+
+message_using_hc       DB      '===============================', cret, newline
+                       DB      'USING YOUR HARDCODED STRING' , cret, newline
+                       DB      '===============================', cret, newline, '$'
+                       
+message_using_input    DB      '===============================', cret, newline
+                       DB      'Please enter your string below' , cret, newline
+                       DB      '===============================', cret, newline, '$'
+                       
+message_try_again      DB      cret, newline, 'Give it one more try? (y/n)', cret, newline, '$'
+
+message_press_key      DB      'Press any key to exit...$'                      
+
+message_display_org    DB      cret, newline, 'Your original string: $'                       
+message_display_enc    DB      cret, 'Encrypted message: $'
+message_display_dec    DB      cret, 'Decrypted message: $'
+message_encrypting     DB      'Encrypting...$'
+message_decrypting     DB      'Decrypting...$'
+                       
 
 
-; Just for reference ------------->  'abcdefghijklmnopqrstuvwxyz'
-encrypt_table_lower DB 97 dup (' '), 'qwertyuiopasdfghjklzxcvbnm'  
-decrypt_table_lower DB 97 dup (' '), 'kxvmcnophqrszyijadlegwbuft'  
+; Just for reference --------------------->  'abcdefghijklmnopqrstuvwxyz'
+encryption_table_lower DB      97 dup (' '), 'qwertyuiopasdfghjklzxcvbnm'  
+decryption_table_lower DB      97 dup (' '), 'kxvmcnophqrszyijadlegwbuft'  
 ; We leave 97(61H) blank spaces before the start of the table
 ; as the ASCII value of 'a' = 61H
                                    
-encrypt_table_upper DB 65 dup (' '), 'QWERTYUIOPASDFGHJKLZXCVBNM'  
-decrypt_table_upper DB 65 dup (' '), 'KXVMCNOPHQRSZYIJADLEGWBUFT'
+encryption_table_upper DB      65 dup (' '), 'QWERTYUIOPASDFGHJKLZXCVBNM'  
+decryption_table_upper DB      65 dup (' '), 'KXVMCNOPHQRSZYIJADLEGWBUFT'
 ; We leave 65(41H) blank spaces before the start of the table
 ; as the ASCII value of 'A' = 41H
 
 
-start:
+start:                 
+  
+                       LEA     DX, message_welcome
+                       MOV     AH, 09
+                       INT     21H                    
+                       MOV     AH, 0
+                       INT     16H
+                       CMP     AL, '2'        ; User chose to use the hardcoded string
+                       JE      use_hc
+                       CMP     AL, '1'        ; User chose to enter a string
+                       JNE     start
+                       CALL    get_input
+                       JMP     start_process
+                       
+use_hc:                LEA     DX, message_using_hc
+                       MOV     AH, 09
+                       INT     21H
+                       LEA     SI, hardcoded_string 
+                       
+                                                                                        
+start_process:
 
-; Encrypt:
-LEA     SI, string1
-CALL    encrypt
+; Display original string
+                       LEA     DX, message_display_org
+                       MOV     AH, 09          ; value of AH is adjusted as operation of int 21H depends on its value
+                       INT     21H             ; at AH = 09, int 21H outputs string at DS:DX
+                       LEA     DX, SI
+                       MOV     AH, 09          
+                       INT     21H             
+                       
+                                                                                                                      
+; Encrypt:             
+                       LEA     DX, message_encrypting   ; Display message
+                       MOV     AH, 09
+                       INT     21H
+                       MOV     AH, 1           ; value of AH is adjusted as operation of encrypt_decrypt depends on its value
+                       CALL    encrypt_decrypt ; AH = 1 means monoalphabetic encryption, 0 means monoalphabetic decryption, else do nothing
 
 ; Display result on the screen:
-LEA     DX, string1
-MOV     AH, 09          ; value of AH is adjusted as operation of int 21H depends on its value
-INT     21H             ; at AH=09, int 21H outputs string at DS:DX
+                       LEA     DX, message_display_enc
+                       MOV     AH, 09          ; value of AH is adjusted as operation of int 21H depends on its value
+                       INT     21H             ; at AH = 09, int 21H outputs string at DS:DX             
+                       LEA     DX, SI
+                       MOV     AH, 09          
+                       INT     21H             
 
 ; Decrypt:
-LEA     SI, string1
-CALL    decrypt
-
+                       LEA     DX, message_decrypting    ; Display message
+                       MOV     AH, 09
+                       INT     21H
+                       MOV     AH, 0           ; AH = 0 means monoalphabetic decryption
+                       CALL    encrypt_decrypt 
+                    
 ; Display result on the screen:
-LEA     DX, string1
-MOV     AH, 09          ; value of AH is adjusted as operation of int 21H depends on its value
-INT     21H             ; at AH=09, int 21H outputs string at DS:DX
+                       LEA     DX, message_display_dec
+                       MOV     AH, 09          ; value of AH is adjusted as operation of int 21H depends on its value
+                       INT     21H             ; at AH = 09, int 21H outputs string at DS:DX             
+                       LEA     DX, SI
+                       MOV     AH, 09          
+                       INT     21H
 
+;Display try again dialogue
+try_again:             LEA     DX, message_try_again    
+                       MOV     AH, 09
+                       INT     21H
+                       MOV     AH, 0
+                       INT     16H
+                       CMP     AL, 'y'
+                       JE      start
+                       CMP     AL, 'n'
+                       JNE     try_again
+                       
+                    
 ; Wait for any key...
-MOV     AH, 0
-INT     16H
-
-
-RET
-
-
-
-;   si - address of string to encrypt
-encrypt PROC NEAR
-
-enc_next_char:
-	
-	MOV     AL, [SI] 
-	CMP     AL, ' '       ;<--- Beginning of space check
-	JNE     cont_enc          ; Since this was a college assignment, One of my requirements was to omit spaces in my result
-	CALL    omit_spaces       ; so you can just remove this section if you do not wish to do that
-    JMP     enc_next_char ;<--- End of space check
-    	
-cont_enc:	
-	CMP     AL, '$'      ; End of string?
-	JE      end_of_string_enc
-	CMP     AL, 'a'
-	JB      check_upper_enc
-	CMP     AL, 'z'
-	JA      skip_enc
-	LEA     BX, encrypt_table_lower 
-	JMP     op_enc
-check_upper_enc:
-    CMP     AL, 'A'
-    JB      skip_enc
-    CMP     AL, 'Z'
-    JA      skip_enc
-    LEA     BX, encrypt_table_upper	
-op_enc:		
-	XLATB
-	MOV     [SI], AL
-skip_enc:
-	INC     SI	
-	JMP     enc_next_char
-
-end_of_string_enc:
-
-
-RET
-encrypt ENDP
-   
-   
+                       LEA     DX, message_press_key
+                       MOV     AH, 09
+                       INT     21H
+                       MOV     AH, 0           ; value of AH is adjusted as operation of int 16H depends on its value
+                       INT     16H             ; at AH = 00, int 16H waits for keystroke from the keyboard (no echo)       
+                    
+                       RET   
    
    
 ;   si - address of string to encrypt
-decrypt PROC NEAR
+encrypt_decrypt        PROC    NEAR
+                       PUSH    SI
+next_char:             MOV     AL, [SI]
+                	   CMP     AL, '$'         ; End of string?
+                	   JE      end_of_string
+                	   	 
+                	   CMP     AL, ' '         ;<--- Beginning of space check
+        	           JNE     cont            ; Since this was a college assignment, One of my requirements was to omit spaces in my result so
+	                   CALL    omit_space      ; you can just remove these 4 lines and the omit_space subroutine if you do not wish to do that.       	           
+                       JMP     next_char       ;<--- End of space check
+                       	
+cont:                  CALL    enc_dec_char
+    	               INC     SI	
+            	       JMP     next_char
+end_of_string:         POP     SI
+                       RET            
+encrypt_decrypt        ENDP
 
-dec_next_char:
-	
-	MOV     AL, [SI] 
-	CMP     AL, ' '       ;<--- Beginning of space check
-	JNE     cont_dec          ; Since this was a college assignment, One of my requirements was to omit spaces in my result
-	CALL    omit_spaces       ; so you can just remove this section if you do not wish to do that
-    JMP     dec_next_char ;<--- End of space check
-    	
-cont_dec:	
-	CMP     AL, '$'      ; End of string?
-	JE      end_of_string_dec
-	CMP     AL, 'a'
-	JB      check_upper_dec
-	CMP     AL, 'z'
-	JA      skip_dec
-	LEA     BX, decrypt_table_lower 
-	JMP     op_dec
-check_upper_dec:
-    CMP     AL, 'A'
-    JB      skip_dec
-    CMP     AL, 'Z'
-    JA      skip_dec
-    LEA     BX, decrypt_table_upper	
-op_dec:		
-	XLATB
-	MOV     [SI], AL
-skip_dec:
-	INC     SI	
-	JMP     dec_next_char
-
-end_of_string_dec:
-
-
-RET
-decrypt ENDP
 
       
-
+;
 ; Subroutine to send space to the end of the string (after '$')
-omit_spaces PROC NEAR    
-    PUSH    SI
-    PUSH    BX             ; The reason I send the space after the '$'
-    MOV     BX, SI         ; is to handle several consecutive spaces without
-	DEC     SI             ; entering an inifnite loop as opposed to just swapping
-omit_spaces_loop:          ; the ' ' character with the following character
-    INC     SI              
-    INC     BX
-    MOV     AL, [BX]
-    MOV     [BX], ' '
-    MOV     [SI], AL
-    CMP     [SI], '$'
-    JNE     omit_spaces_loop
-    POP     BX
-    POP     SI
-    
-    RET
-omit_spaces ENDP
-        
+; You can delete this subroutine if you do not need it
+;
+omit_space             PROC    NEAR    
+                       PUSH    SI              ; The reason I send the space after the '$'
+	                                           ; is to handle several consecutive spaces without
+omit_space_loop:       MOV     AL, [SI+1]      ; entering an inifnite loop as opposed to just swapping
+                       MOV     [SI+1], ' '     ; the ' ' character with the character after it 
+                       MOV     [SI], AL
+                       INC     SI
+                       CMP     [SI-1], '$'                      
+                       JNE     omit_space_loop
+                       POP     SI  
+                       RET
+omit_space             ENDP    
+                   
+                   
+                   
+; Subroutine to convert character with the
+; appropriate table (encrypt/decrypt)(uppercase/lowercase)
+enc_dec_char           PROC    NEAR
+                       PUSH    BX    
+                       CMP     AL, 'a'       
+                       JB      check_upper_char
+                       CMP     AL, 'z'
+                       JA      skip_char
+                       CMP     AH, 1          ; AH = 1 means monoalphabetic encryption, since we are working with lower case, we use encryption_table_lower
+                       JE      encrypt_lower_char
+                       CMP     AH, 0          ; AH = 0 means monoalphabetic decryption, since we are working with lower case, we use decryption_table_lower
+                       JNE     skip_char
+                       LEA     BX, decryption_table_lower
+                       JMP     translate_char                     
+encrypt_lower_char:	   LEA     BX, encryption_table_lower 
+                	   JMP     translate_char                	
+check_upper_char:      CMP     AL, 'A'
+                       JB      skip_char
+                       CMP     AL, 'Z'
+                       JA      skip_char
+                       CMP     AH, 1          ; AH = 1 means monoalphabetic encryption, since we are working with upper case, we use encryption_table_upper
+                	   JE      encrypt_upper_char
+                	   CMP     AH, 0          ; AH = 0 means monoalphabetic decryption, since we are working with upper case, we use decryption_table_upper
+                       JNE     skip_char
+                       LEA     BX, decryption_table_upper
+                       JMP     translate_char 
+encrypt_upper_char:    LEA     BX, encryption_table_upper	
+translate_char:        XLATB
+	                   MOV     [SI], AL	                		
+skip_char:             POP     BX
+                       RET
+enc_dec_char           ENDP
 
+
+
+; Subroutine to take input string from the user
+; The subroutine handles if the user presses backspace: delete char + inc CX
+; The subroutine allows the user to enter a maximum of 256 chars 
+get_input              PROC    NEAR
+                       LEA     DX, message_using_input
+                       MOV     AH, 09
+                       INT     21H
+                       LEA     SI, input_string
+                       MOV     AH, 1
+                       MOV     CX, 255        ; To set a cap for string input to 256 chars            
+                       JMP     input_loop
+backspace_entered:     INC     CX             ; Increment CX in case user presses backspace as a character is deleted
+input_loop:            INT     21H                                                      
+                       MOV     [SI], AL
+                       CMP     AL, bcksp
+                       JNE     cont_input                     
+                       CMP     SI, offset input_string  
+                       JE      input_loop     ;If the string is empty just loop again without affecting SI and without incrementing CX                    
+                       MOV     [SI], ' '
+                       CALL    omit_space
+                       DEC     SI                      
+                       JMP     backspace_entered                                            
+cont_input:            INC     SI
+                       CMP     AL, cret
+                       JE      terminate_string                   
+                       LOOP    input_loop     ; LOOP instead of JMP to incorporate (CX != 0000H) as a jump condition                                              
+terminate_string:      MOV     [SI-1], cret
+                       MOV     [SI], newline
+                       MOV     [SI+1], '$'
+                       LEA     SI, input_string
+                       RET
+get_input              ENDP
+        
 end
        
